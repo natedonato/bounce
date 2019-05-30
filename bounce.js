547f54b96ced6016775945ee5d80fda93f35171e
Nate Donato <natedonato@gmail.com>
2019-05-30 12:32:16 -0700

    testing

--- bounce.js
+++ bounce.js
@@ -8,26 +8,28 @@ document.addEventListener("DOMContentLoaded", () =>{
     ctx = canvas.getContext("2d");
     ctx.fillRect(50,50, 100, 100);
 
-    const gameView = new GameView(0, ctx, canvas.height, canvas.width).bindtilt();
+    const gameView = new GameView(0, ctx, canvas.height, canvas.width).start();
 });
 
 
 class GameView {
     constructor(game, ctx, canvasheight, canvaswidth) {
+        this.ctx = ctx;
         this.canvasheight = canvasheight;
         this.canvaswidth = canvaswidth;
-        this.object = {};
+        this.object = {pos: {x: 50, y: 50}, vel: {x: 0, y: 0}, w: 20, h: 20, maxspeed: 8};
         this.lastUpdated = 0;
+
     }
 
     bindtilt() {
 
 
-        window.addEventListener("deviceorientation", this.handleTilt);
+        window.addEventListener("deviceorientation", this.recordTilt);
 
     }
 
-    handleTilt(event) {
+    recordTilt(event) {
         let x = event.beta;
         let y = event.gamma;
 
@@ -36,11 +38,41 @@ class GameView {
 
         x += 90;
         y += 90;
-        document.getElementById("x").innerHTML = x;
-        document.getElementById("y").innerHTML = y;
+        this.x = x;
+        this.y = y;
+    }
+
+    handleTilt(){
+        this.object.vel.x = 5 * (this.x - 90)/90;
+        this.object.vel.y = 5 * (this.y - 90)/90;
+    }
+
+    updateObjectPos(dt){
+        this.object.pos.x += this.object.vel.x * dt;
+        this.object.pos.y += this.object.vel.y * dt;
+        
+        if(this.object.pos.x < 0){this.object.pos.x = 0;}
+        else if(this.object.pos.x + this.object.w > this.canvaswidth){this.object.pos.x = this.canvaswidth - this.object.w;}
+
+        if(this.object.pos.y < 0){this.object.pos.y = 0;}
+        else if(this.object.pos.y + this.object.h > this.canvasheight){this.object.pos.y = this.canvasheight - this.object.h;}
+
+        this.ctx.fillRect(this.object.pos.x, this.object.pos.y, this.object.w, this.object.h);
 
     }
 
+    update(timestamp) {
+        const dt = (timestamp - this.lastUpdated) / 10;
+        this.handleTilt();
+        this.updateObjectPos(dt);
+
+        this.lastUpdated = timestamp;
+        requestAnimationFrame(this.update.bind(this));
+    }
 
 
+    start() {
+        this.bindtilt();
+        this.update(0);
+    }
 }
