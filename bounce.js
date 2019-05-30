4d5068da9316cfbfbfe42bd468796a626eb6cee5
Nate Donato <natedonato@gmail.com>
2019-05-30 16:43:46 -0700

    actually friction is stupid

--- bounce.js
+++ bounce.js
@@ -17,12 +17,11 @@ class GameView {
         this.ctx = ctx;
         this.canvasheight = canvasheight;
         this.canvaswidth = canvaswidth;
-        this.object = {pos: {x: 50, y: 50}, vel: {x: 0, y: 0}, w: 20, h: 20, maxspeed: 10};
+        this.object = {pos: {x: 50, y: 50}, vel: {x: 0, y: 0}, w: 20, h: 20, maxspeed: 16};
         this.lastUpdated = 0;
         this.x = 90;
         this.y = 90;
         this.handleTilt = this.handleTilt.bind(this);
-        this.friction = 0.95;
     }
 
 
@@ -49,9 +48,13 @@ class GameView {
 
     updatevel(dt){
         this.object.vel.x += dt * 8 * (this.x - 90)/90;
+
         if(this.object.vel.x > 10){this.object.vel.x = 10;}
+        
         if(this.object.vel.x < -10){this.object.vel.x = -10;}
+        
         this.object.vel.y += dt * 8 * (this.y - 90)/90;
+        
         if (this.object.vel.y > 10) { this.object.vel.y = 10; }
         if (this.object.vel.y < -10) { this.object.vel.y = -10; }
     }
@@ -59,8 +62,6 @@ class GameView {
     updateObjectPos(dt){
         this.object.pos.x += this.object.vel.x * dt;
         this.object.pos.y += this.object.vel.y * dt;
-        // this.object.vel.x *= this.friction;
-        // this.object.vel.y *= this.friction;
 
         
         if(this.object.pos.x < 0){this.object.pos.x = 0; this.object.vel.x = -this.object.vel.x * 0.8;}
